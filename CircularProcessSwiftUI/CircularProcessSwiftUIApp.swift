cf653d39aaca4d692a2a3cc9ecd47db617f8d26a
fatihfilizol <fatihfilizol@gmail.com>
2022-11-14 09:11:38 +0300

    Initial Commit

---
+++ CircularProcessSwiftUI/CircularProcessSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProcessSwiftUIApp.swift
+//  CircularProcessSwiftUI
+//
+//  Created by Fatih Filizol on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CircularProcessSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
